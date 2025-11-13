3fe16cde654e8854b070c95f04e52831f0d733e4
gNOR-mu <gabriel.norambuenam@gmail.com>
2025-11-12 21:07:56 -0300

    cambio rutas imagenes

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,14 +1,14 @@
 export const products = [
     {
         title: "Bergans jacket",
-        imgSrc: "/assets/img/960px-OutDoor_2018,_Friedrichshafen_(1X7A0457).jpg",
+        imgSrc: "assets/img/960px-OutDoor_2018,_Friedrichshafen_(1X7A0457).jpg",
         link: "#",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam autem est doloremque, odio minima doloresrepellat unde fugiat rem et asperiores exercitationem aspernatur! Natus, voluptas perferendis a temporibus sed dolore!"
     },
 
     {
         title: "Kit de ropa para la nieve",
-        imgSrc: "/assets/img/mountain-snow-cold-winter-leather-clothing-856093-pxhere.com.jpg",
+        imgSrc: "assets/img/mountain-snow-cold-winter-leather-clothing-856093-pxhere.com.jpg",
         link: "#",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam autem est doloremque, odio minima doloresrepellat unde fugiat rem et asperiores exercitationem aspernatur! Natus, voluptas perferendis a temporibus sed dolore!"
 
@@ -16,7 +16,7 @@ export const products = [
 
     {
         title: "Mochila de montaña",
-        imgSrc: "/assets/img/nature-forest-walking-mountain-hiking-adventure-1395064-pxhere.com.jpg",
+        imgSrc: "assets/img/nature-forest-walking-mountain-hiking-adventure-1395064-pxhere.com.jpg",
         link: "#",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam autem est doloremque, odio minima doloresrepellat unde fugiat rem et asperiores exercitationem aspernatur! Natus, voluptas perferendis a temporibus sed dolore!"
     }
