e00ddeddaccbc7b1e8248ad5236062e5a76bb9f8
gNOR-mu <gabriel.norambuenam@gmail.com>
2025-11-13 03:57:07 -0300

    avances página de productId y corrección menor cita en inicio

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,15 +2,13 @@ import { products } from "./products.js";
 
 
 products.forEach(product => {
-    const description = product.description.length > 80 ? product.description.substring(0, 80) + "..." : product.description;
-
     $("#products-container").append(`
     <div class="col-lg card p-0">
         <img src="${product.imgSrc}" class="card-img-top product-card-img" alt="Producto destacado: ${product.title}">
         <div class="card-body">
             <h5 class="card-title">${product.title}</h5>
-            <p class="card-text">${description}</p>
-            <a href="${product.link}" class="btn btn-dark">Visitar producto</a>
+            <p class="card-text">${product.shortDescription}</p>
+            <a href="productId.html?id=${product.id}" class="btn btn-dark">Visitar producto</a>
         </div>
     </div>
 `);
