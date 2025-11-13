72c454a82c65ecee171f0d25bb947925f7ef3d69
gNOR-mu <gabriel.norambuenam@gmail.com>
2025-11-13 19:49:03 -0300

    avances localstorage product

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,6 +1,14 @@
-// Carga el navbar en su placeholder
-$("#header-placeholder").load("components/header.html", function () {
+import { getTotalProductsInCart } from "./products.js";
 
-    // Carga el footer en su placeholder
+export function updateProductTotal() {
+    const productTotal = $("#product-total");
+    const totalProducts = getTotalProductsInCart();
+    productTotal.text(totalProducts);
+}
+
+$("#header-placeholder").load("components/header.html", function () {
     $("#footer-placeholder").load("components/footer.html");
+    updateProductTotal();
 });
+
+$(document).on("cartUpdated", updateProductTotal);
--- assets/js/products.js
+++ assets/js/products.js
@@ -1,3 +1,6 @@
+// localstorage {id:... , quantity}
+// guarda en localstorage
+
 export const products = [
     {
         id: 0,
@@ -62,3 +65,54 @@ El diseño clásico con lentes y marco negros las convierte en un accesorio vers
     },
 ]
 
+//obtiene el numero total de productos en carrito
+export function getTotalProductsInCart() {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    return cart.reduce((total, product) => total + product.quantity, 0);
+}
+
+// Agrega un producto a localstorage, de 1 en 1
+export function addProduct(product) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    const productIndex = cart.findIndex(p => p.id === product.id);
+    if (productIndex > -1) {
+        cart[productIndex].quantity++;
+    } else {
+        cart.push({ id: product.id, quantity: 1 });
+    }
+    localStorage.setItem('cart', JSON.stringify(cart));
+    document.dispatchEvent(new Event('cartUpdated'));
+
+}
+
+// Resta un producto de localstorage, de 1 en 1
+export function subtractProduct(id) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    const productIndex = cart.findIndex(p => p.id === id);
+    if (productIndex === -1) {
+        return
+    }
+
+    cart[productIndex].quantity--;
+    if (cart[productIndex].quantity === 0) {
+        cart.splice(productIndex, 1);
+    }
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+    document.dispatchEvent(new Event('cartUpdated'));
+}
+
+// elimina completamente un producto de localstorage
+export function removeProduct(id) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    cart = cart.filter(p => p.id !== id);
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+
+//obtiene quantity de un producto
+export function getProductQuantity(id) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    const product = cart.find(p => p.id === id);
+    return product ? product.quantity : 0;
+}
