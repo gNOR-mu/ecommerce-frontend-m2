4a869941e351fa3556ae65cabf1a4f502ce699e9
gNOR-mu <82237339+gNOR-mu@users.noreply.github.com>
2025-11-13 21:22:04 -0300

    card productos modificados y creación de página de productos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,18 @@
 import { products } from "./products.js";
 
+const productsContainer = $("#products-container");
+
+
 // Primeros 3 elementos
 products.slice(0, 3).forEach(product => {
-    $("#products-container").append(`
-    <div class="col-lg card p-0">
-        <img src="${product.imgSrc}" class="card-img-top product-card-img" alt="Producto destacado: ${product.title}">
-        <div class="card-body">
-            <h5 class="card-title">${product.title}</h5>
-            <p class="card-text">${product.shortDescription}</p>
-            <a href="productId.html?id=${product.id}" class="btn btn-dark">Visitar producto</a>
-        </div>
-    </div>
+    productsContainer.append(`
+        <a href="productId.html?id=${product.id}" class="col-lg card p-0 product-card-link">
+            <img src="${product.imgSrc}" class="card-img-top product-card-img" alt="Producto destacado: ${product.title}">
+            <div class="card-body">
+                <h5 class="card-title">${product.title}</h5>
+                <p class="card-text">${product.shortDescription}
+                </p>
+            </div>
+        </a>
 `);
 });
